4b9a572c87c508af432f774433b79f94bb6369fe
arthurlegal552 <arthurcostagoncalves8@gmail.com>
2025-05-09 09:32:45 -0300

    welcom.html

--- talk.js
+++
@@ -1,8 +0,0 @@
-const fala = document.getElementById("fala");
-const frases = ["yo", "are you still up?", "how about get more zazas?", "whatever"];
-let index = 0;
-
-setInterval(() => {
-    fala.textContent = frases[index];  // Atualiza a fala
-    index = (index + 1) % frases.length;  // Muda para a próxima frase
-}, 20000);  // 20 segundos
